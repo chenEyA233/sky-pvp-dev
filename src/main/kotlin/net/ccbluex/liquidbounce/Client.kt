48d37ea8582ea0f9f7a3e2726930910d43c572cc
chenEyA233 <340063685@qq.com>
2025-05-02 19:49:34 +0800

    up to v1.1

--- src/main/kotlin/net/ccbluex/liquidbounce/Client.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/Client.kt
@@ -12,9 +12,9 @@ class SlienceFix
 public class Client{
     companion object {
         const val CLIENT_NAME = "SKY PVP Client"//客户端名称:)
-        const val CLIENT_VER = "1.0"//客户端版本
+        const val CLIENT_VER = "1.1"//客户端版本
         const val CLIENT_AUTHOR = "chenEyA"//作者
-        const val TEXT = "SKY PVP Client | v1.0 | by:chenEyA"//完整版（测试，未使用）
+        const val TEXT = "SKY PVP Client | v1.1 | by:chenEyA"//完整版（测试，未使用）
         const val SlienceFix = "Hallo world! |"//你好，世界！
         const val iuguduhg = "114514!!!"//114514
     }
